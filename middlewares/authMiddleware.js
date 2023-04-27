2191aca774b3862d237d0192def902ab6e1e2cca
alborzasd <alborzasd@gmail.com>
2023-04-27 11:44:25 +0330

    Continue refactore env variables (authMiddleware.js)

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const { JwtSecret } = require('../config');
 
 const requireAuth = (req, res, next) => {
 
     const token = req.cookies.jwt;
 
     if(token){
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        jwt.verify(token, JwtSecret, (err, decodedToken) => {
             if(err) {
                 return res.status(401).json({message: err.message}); // {message: invalid token, jwt expired, ...}
                 // TODO: close socket connection if token expired ?
